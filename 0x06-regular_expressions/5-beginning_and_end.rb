4e10ca5416f2f9de2b4fc491f7c77c8c04201539
ja-programmer <1270@holbertonschool.com>
2020-02-24 08:19:46 -0500

    task5

---
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+# shebang line - The first match in the $PATH environment variable.
+# Ruby script that accepts one argument and
+# pass it to a regular expression matching method
+puts ARGV[0].scan(/h.n/).join
